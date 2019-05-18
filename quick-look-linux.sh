3928e4f7fab010b4cec5891182abe887425149ec
sharkyReal <probro1802@gmail.com>
2019-05-18 17:02:13 +0000

    Quick Look for Linux!

---
+++ quick-look-linux.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo Loading
+
+echo Welcome to the linux verison of "Quick Look for Termcmd". If you get a error please update your python to python 3x.
+
+echo TERMCMD-LINUX V.QL-LINUX0.1
+python3 main.py
+
